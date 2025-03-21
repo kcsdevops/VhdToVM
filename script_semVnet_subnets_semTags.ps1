3fe120ba76ad477fafd279d7c11fc27f062df029
Max-Surprise-MFK <129492553+kcsdevops@users.noreply.github.com>
2025-03-21 16:41:54 -0300

    Update script_semVnet_subnets_semTags.ps1

--- script_semVnet_subnets_semTags.ps1
+++ script_semVnet_subnets_semTags.ps1
@@ -36,49 +36,45 @@ $tags = @{
     "Backup" = "Enabled"
 }
 
-# Crie um grupo de recursos
-New-AzResourceGroup -Name $resourceGroupName -Location $location
+# Crie um grupo de recursos (se necessário):
+New-AzResourceGroup -Name $resourceGroupName -Location $location -Tag $tags
 
-# Crie uma conta de armazenamento
-New-AzStorageAccount -ResourceGroupName $resourceGroupName -Name $storageAccountName -Location $location -SkuName "Standard_LRS"
+# Crie uma conta de armazenamento:
+New-AzStorageAccount -ResourceGroupName $resourceGroupName -Name $storageAccountName -Location $location -SkuName "Standard_LRS" -Tag $tags
 
-# Obtenha a chave de acesso da conta de armazenamento
+# Obtenha a chave de acesso da conta de armazenamento:
 $storageAccountKey = (Get-AzStorageAccountKey -ResourceGroupName $resourceGroupName -Name $storageAccountName).Value[0]
 $context = New-AzStorageContext -StorageAccountName $storageAccountName -StorageAccountKey $storageAccountKey
 
-# Crie um contêiner de armazenamento
+# Crie um contêiner de armazenamento:
 New-AzStorageContainer -Name $storageContainerName -Context $context
 
-# Carregue o VHD para o contêiner de armazenamento (se necessário)
+# Carregue o VHD para o contêiner de armazenamento (se necessário):
 Set-AzStorageBlobContent -File "C:\caminho\para\seu\vhd.vhd" -Container $storageContainerName -Blob "meuvhd.vhd" -Context $context
 
-# Crie um disco gerenciado a partir do VHD
-$diskConfig = New-AzDiskConfig -AccountType "Standard_LRS" -Location $location -CreateOption "Import" -SourceUri $vhdUri
-$disk = New-AzDisk -ResourceGroupName $resourceGroupName -DiskName "MeuDisco" -Disk $diskConfig
-
-#Crie uma rede virtual e uma sub-rede (se não houver ou):
-$vnet = New-AzVirtualNetwork -ResourceGroupName $resourceGroupName -Location $location -Name $vnetName -AddressPrefix "10.0.0.0/16"
-$subnet = Add-AzVirtualNetworkSubnetConfig -Name $subnetName -AddressPrefix "10.0.0.0/24" -VirtualNetwork $vnet
-$vnet | Set-AzVirtualNetwork
+# Crie um disco gerenciado SSD de 2,5 TB a partir do VHD:
+$diskConfig = New-AzDiskConfig -AccountType "Premium_LRS" -Location $location -CreateOption "Import" -SourceUri $vhdUri -DiskSizeGB $diskSizeGB
+$disk = New-AzDisk -ResourceGroupName $resourceGroupName -DiskName "MeuDisco" -Disk $diskConfig -Tag $tags
 
 # Obtenha a VNet e a Subnet existentes:
 $vnet = Get-AzVirtualNetwork -ResourceGroupName $resourceGroupName -Name $vnetName
 $subnet = Get-AzVirtualNetworkSubnetConfig -VirtualNetwork $vnet -Name $subnetName
 
+# Crie uma interface de rede com um IP privado:
+$nic = New-AzNetworkInterface -ResourceGroupName $resourceGroupName -Location $location -Name $nicName -SubnetId $subnet.Id -PrivateIpAddress $privateIpAddress -Tag $tags
 
-#Crie uma interface de rede com um IP privado:
-$nic = New-AzNetworkInterface -ResourceGroupName $resourceGroupName -Location $location -Name $nicName -SubnetId $subnet.Id -PrivateIpAddress $privateIpAddress
-
-
-# Crie a configuração da VM
+# Crie a configuração da VM:
 $vmConfig = New-AzVMConfig -VMName $vmName -VMSize $vmSize | `
     Set-AzVMOperatingSystem -Linux -ComputerName $vmName -Credential (New-Object System.Management.Automation.PSCredential($adminUsername, (ConvertTo-SecureString $adminPassword -AsPlainText -Force))) | `
     Set-AzVMSourceImage -Id $disk.Id | `
     Add-AzVMNetworkInterface -Id $nic.Id
+    
+# Crie a VM:
+New-AzVM -ResourceGroupName $resourceGroupName -Location $location -VM $vmConfig -Tag $tags
 
-# Crie a VM
-New-AzVM -ResourceGroupName $resourceGroupName -Location $location -VM $vmConfig
-
-# Destrua o contêiner de blob
+# Destrua o contêiner de blob:
 Remove-AzStorageContainer -Name $storageContainerName -Context $context
 
+
+
+# Certifique-se de substituir as variáveis pelos valores apropriados para o seu ambiente. Aqui está o script completo:
